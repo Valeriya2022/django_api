02095c40734ed080e52dfd5ca5feb9a53392bd32
Valeriya <nikavella2022@gmail.com>
2022-01-19 16:48:01 +0600

    added Test model with 4 fields

--- django_api/tutorial/models.py
+++ django_api/tutorial/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class TestModel(models.Model):
+    name = models.CharField(max_length=255, unique=True, null=True, blank=True )
+    description = models.TextField()
+    phone_number = models.PositiveIntegerField()
+    is_live = models.BooleanField()
+    amount = models.FloatField()
+# name = models.TextField(error_messages={"null":"this field cannot be null",
+#     "blank":"this field cannot be null"})
+# name = models.IntegerField()
+# name = models.BigIntegerField()
+# name = models.PositiveIntegerField()
+# name = models.FloatField()
+# name = models.BooleanField()
+# name = models.ManyToManyField()
+# name = models.OneToOneField()
+# name = models.ForeignKey()
